7c7da5fc87fc0533c666575fe6c4e324f1e6f358
Tom Blackshaw <tom.blackshaw@protonmail.com>
2021-10-17 19:56:16 +0200

    Still testing

--- src/main.py
+++ src/main.py
@@ -4,20 +4,25 @@ Created on Oct 16, 2021
 @author: Tom Blackshaw
 
 '''
-from my.partitiontools import Disk
+# from my.partitiontools import *
 
-'''
+# if __name__ == '__main__':
 
-'''
+from my.partitiontools import *
+d = Disk('/dev/sda')
+d.delete_all_partitions()
+d.add_partition(partno=1, start=50000, end=99999)
+d.add_partition(partno=2, start=0, end=d.partitions[-1].start)
+d.add_partition(partno=2, start=0, end=d.partitions[-1].start - 1)
+d.delete_partition(2)
+d.add_partition(2, size_in_MiB=400)
+d.add_partition(3, size_in_MiB=300)
+d.add_partition(4, size_in_MiB=200)
+d.delete_partition(3)
+d.add_partition(3)
+d.add_partition(3, start=d.partitions[1].end + 1)
 
-if __name__ == '__main__':
-    d = Disk('/dev/sda')
-    d.delete_all_partitions()
-    d.add_partition(1, 50000, 99999)
-    d.add_partition(2, 0, d.partitions[0].start)
-    d.add_partition(2, 0, d.partitions[0].start - 1)
-    d.delete_partition(2)
-    d.add_partition(2, d.partitions[0].end + 1, d.partitions[0].end + 99999)
-    d.add_partition(3, d.partitions[1].end + 1, d.partitions[1].end + 99999)
-    d.add_partition(4, d.partitions[2].end + 1, d.partitions[2].end + 99999)
-    d.delete_partition(3)
+[r for r in d.partitions if r.node[-1:] == str(1)]
+
+d.delete_partition(3)
+d.add_partition(3)
