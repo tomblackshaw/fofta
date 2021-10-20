f58d1068c647f954c84bb7957b3986f048df0d4a
Tom Blackshaw <tom.blackshaw@protonmail.com>
2021-10-20 14:57:58 +0200

    Tidied up main.py

--- src/main.py
+++ src/main.py
@@ -5,24 +5,17 @@ Created on Oct 16, 2021
 
 '''
 # from my.partitiontools import *
+from my.partitiontools import get_list_of_all_disks
 
 # if __name__ == '__main__':
 
-from my.partitiontools import *
-d = Disk('/dev/sda')
-d.delete_all_partitions()
-d.add_partition(partno=1, start=50000, end=99999)
-d.add_partition(partno=2, start=0, end=d.partitions[-1].start)
-d.add_partition(partno=2, start=0, end=d.partitions[-1].start - 1)
-d.delete_partition(2)
-d.add_partition(2, size_in_MiB=400)
-d.add_partition(3, size_in_MiB=300)
-d.add_partition(4, size_in_MiB=200)
-d.delete_partition(3)
-d.add_partition(3)
-d.add_partition(3, start=d.partitions[1].end + 1)
-
-[r for r in d.partitions if r.node[-1:] == str(1)]
-
-d.delete_partition(3)
-d.add_partition(3)
+'''
+lst = get_list_of_all_disks()
+unmatched_devs = []
+for line in [t for t in txt.split('\n')]:
+    dev = line.split(' ')[-1]
+    if dev != '' \
+    and os.path.exists(os.path.join('/dev', dev)) \
+    and dev not in ([os.path.basename(p.node) for p in d.partitions]):
+        unmatched_devs.append(dev)
+'''
