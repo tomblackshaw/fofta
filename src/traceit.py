7fd49b3c7e8d98779fd3398f5d0ed217dfbd0251
Tom Blackshaw <tom.blackshaw@protonmail.com>
2021-11-13 13:27:28 +0100

    Updated traceit.py

--- src/traceit.py
+++ src/traceit.py
@@ -7,6 +7,9 @@ Created on Nov 2, 2021
 #from my.disktools.partitions import DiskPartition, partition_namedtuple
 import os
 from my.globals import call_binary, _GPT, _DOS
+from my.disktools.disks import Disk, get_serno
+from test import MY_TESTDISK_PATH
+import sys
 
 
 
@@ -14,12 +17,22 @@ from my.globals import call_binary, _GPT, _DOS
 
 
 if __name__ == '__main__':
-    os.environ['PATH'] = os.environ['PATH']+':'+os.getenv('PATH')
-    import pydevd; pydevd.settrace("192.168.0.139", port=5678, stdoutToServer=True, stderrToServer=True)
+#    os.environ['PATH'] = os.environ['PATH']+':'+os.getenv('PATH')
+#    import pydevd; pydevd.settrace("192.168.0.139", port=5678, stdoutToServer=True, stderrToServer=True)
+    d = Disk(MY_TESTDISK_PATH, _GPT)
+    shouldbe_serno = get_serno(d.node)
+    actuallyis_serno = d.serno
+    assert(shouldbe_serno == actuallyis_serno)
+#    os.system("sync;sync;sync")
+    d.serno = d.serno.replace('9','0').replace('4','7').replace('8','3')
+#    os.system("sync;sync;sync")
+    new_one = get_serno(d.node)
+    assert(new_one == d.serno)
+    sys.exit(0)
+    
     MY_DISK_IMGSIZE_IN_MB = 160
     MY_DISK_LOOPDEV  = '/dev/loop5'
     MY_DISK_IMGFILE = '/tmp/.fofta.temp.image.file'
-    from my.disktools.disks import Disk
     s = call_binary(['df','-m','/tmp'])[1].split('\n')[1].split('tmpfs')[1].strip(' ')
     if int(s.split(' ')[0]) < MY_DISK_IMGSIZE_IN_MB:
         MY_DISK_IMGFILE = MY_DISK_IMGFILE.replace('/tmp/','/root/')
